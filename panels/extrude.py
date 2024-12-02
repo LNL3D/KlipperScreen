e0d2ac684b3c1251c6f8544a13e4e9fb27d64b0d
LNL3D Robert <robert@lnl3d.com>
2024-12-02 15:43:56 -0800

    extrude panel customizations - speed limits

--- panels/extrude.py
+++ panels/extrude.py
@@ -23,7 +23,7 @@ class ExtrudePanel(ScreenPanel):
         self.load_filament = any("LOAD_FILAMENT" in macro.upper() for macro in macros)
         self.unload_filament = any("UNLOAD_FILAMENT" in macro.upper() for macro in macros)
 
-        self.speeds = ['1', '2', '5', '25']
+        self.speeds = ['1', '2', '5', '10']
         self.distances = ['5', '10', '15', '25']
         if self.ks_printer_cfg is not None:
             dis = self.ks_printer_cfg.get("extrude_distances", '5, 10, 15, 25')
@@ -31,7 +31,7 @@ class ExtrudePanel(ScreenPanel):
                 dis = [str(i.strip()) for i in dis.split(',')]
                 if 1 < len(dis) < 5:
                     self.distances = dis
-            vel = self.ks_printer_cfg.get("extrude_speeds", '1, 2, 5, 25')
+            vel = self.ks_printer_cfg.get("extrude_speeds", '1, 2, 5, 10')
             if re.match(r'^[0-9,\s]+$', vel):
                 vel = [str(i.strip()) for i in vel.split(',')]
                 if 1 < len(vel) < 5:
